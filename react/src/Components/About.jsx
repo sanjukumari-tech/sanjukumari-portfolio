5421646a23a359d0ba48ff558a9130d1f99ac94f
sanjukumari-tech <foodlazzez@gmail.com>
2024-09-04 17:31:51 +0530

    this is updated portfolio

--- react/src/Components/About.jsx
+++ react/src/Components/About.jsx
@@ -30,14 +30,14 @@ const About = ({ref,func}) => {
                 </div>
                 <div >
                     <p id="user-detail-intro">Passionate and Results-Driven Web Developer with over 1000+ hours of hands-on coding experience and a robust foundation in data structures and algorithms (100+ hours). Adept at both front-end and back-end development, with expertise in ReactJS, Node.js, JavaScript, ExpressJS, and MongoDB. Demonstrated proficiency in building dynamic web applications, including feature-rich clones of popular websites. Excel in collaborative environments, consistently delivering high-quality solutions on time. A highly motivated problem solver with a disciplined approach to tackling complex challenges and a commitment to continuous learning and improvement</p>
-                    <div className="resume-button" id="resume-button-2">
-                    <button className="download-button" onClick={handleDownload}>
-                        <div className="docs"><svg className="css-i6dzq1" strokeLinejoin="round" strokeLinecap="round" fill="none" strokeWidth="2" stroke="currentColor" height="20" width="20" viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line y2="13" x2="8" y1="13" x1="16"></line><line y2="17" x2="8" y1="17" x1="16"></line><polyline points="10 9 9 9 8 9"></polyline></svg> Resume</div>
-                        <div className="download">
+                    {/* <div className="resume-button" id="resume-button-2"> */}
+                    {/* <button className="download-button" onClick={handleDownload}> */}
+                        {/* <div className="docs"><svg className="css-i6dzq1" strokeLinejoin="round" strokeLinecap="round" fill="none" strokeWidth="2" stroke="currentColor" height="20" width="20" viewBox="0 0 24 24"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line y2="13" x2="8" y1="13" x1="16"></line><line y2="17" x2="8" y1="17" x1="16"></line><polyline points="10 9 9 9 8 9"></polyline></svg> Resume</div> */}
+                        {/* <div className="download"> */}
                             {/* <svg className="css-i6dzq1" strokeLinejoin="round" strokeLinecap="round" fill="none" strokeWidth="2" stroke="currentColor" height="24" width="24" viewBox="0 0 24 24"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line y2="3" x2="12" y1="15" x1="12"></line></svg> */}
-                        </div>
-                    </button>
-                    </div>
+                        {/* </div> */}
+                    {/* </button> */}
+                    {/* </div> */}
                 </div>
             </div>
 
